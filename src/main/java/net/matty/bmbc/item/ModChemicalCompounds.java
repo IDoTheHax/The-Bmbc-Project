3528574c053440c4b3da6981fb160eaa378b6b02
IDoTheHax <mm.mcleod-illert@gmail.com>
2023-04-23 14:50:03 +0200

    - Added phosphorus, calcium, carnallite, dolomite,  evaporite, magnesite, magnesium, pegmatite, phosphorite, potassium silicon crystals, sodium, sulfur crystal and sulfur powder

--- src/main/java/net/matty/bmbc/item/ModChemicalCompounds.java
+++ src/main/java/net/matty/bmbc/item/ModChemicalCompounds.java
@@ -13,8 +13,14 @@ public class ModChemicalCompounds {
             DeferredRegister.create(ForgeRegistries.ITEMS, BetterMineBetterCraft.MOD_ID);
 
     // Chemical Compounds
+    // alumina
     public static final RegistryObject<Item> ALUMINA = ITEMS.register("alumina",
-            () -> new MineralWithToolTipItem(new Item.Properties().tab(ModChemicalCompoundsCreativeModeTab.CHEMICAL_COMPOUNDS), "Chem. Formula: ", "Al₂O₃"));
+            () -> new MineralWithToolTipItem(new Item.Properties().tab(ModChemicalCompoundsCreativeModeTab.CHEMICAL_COMPOUNDS),
+                    "Also known as aluminum oxide\nChem. Formula: ", "Al₂O₃"));
+
+    public static final RegistryObject<Item> SILICA = ITEMS.register("silica",
+            () -> new MineralWithToolTipItem(new Item.Properties().tab(ModChemicalCompoundsCreativeModeTab.CHEMICAL_COMPOUNDS),
+                    "Also known as silicon dioxide\nChem. Formula: ", "SiO₂"));
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
--- src/main/java/net/matty/bmbc/item/ModMineralItems.java
+++ src/main/java/net/matty/bmbc/item/ModMineralItems.java
@@ -25,18 +25,99 @@ public class ModMineralItems {
             () -> new MineralWithToolTipItem(new Item.Properties().tab(ModResourcesCreativeModeTab.ORES_AND_MINERALS)
                     ,"Chem. Formula: ", "B"));
 
+    // Calcium (solid metal)
+    public static final RegistryObject<Item> CALCIUM = ITEMS.register("calcium",
+            () -> new MineralWithToolTipItem(new Item.Properties().tab(ModResourcesCreativeModeTab.ORES_AND_MINERALS)
+                    ,"Chem. Formula: ", "Ca"));
+
     public static final RegistryObject<Item> LITHIUM = ITEMS.register("lithium_ingot",
             () -> new MineralWithToolTipItem(new Item.Properties().tab(ModResourcesCreativeModeTab.ORES_AND_MINERALS)
                     ,"Chem. Formula: ", "Li"));
+
+    // Magnesium
+    public static final RegistryObject<Item> MAGNESIUM = ITEMS.register("magnesium_ingot",
+            () -> new MineralWithToolTipItem(new Item.Properties().tab(ModResourcesCreativeModeTab.ORES_AND_MINERALS)
+                    ,"Chem. Formula: ", "Mg"));
+
+    // Phosphorus
+    // White Phosphorus (translucent waxy solid)
+    public static final RegistryObject<Item> WHITE_PHOSPHORUS = ITEMS.register("white_phosphorus",
+            () -> new MineralWithToolTipItem(new Item.Properties().tab(ModResourcesCreativeModeTab.ORES_AND_MINERALS)
+                    , "Chem. Formula: ", "P₄"));
+
+    // Red Phosphorus (crystalline material)
+    public static final RegistryObject<Item> RED_PHOSPHORUS = ITEMS.register("red_phosphorus",
+            () -> new MineralWithToolTipItem(new Item.Properties().tab(ModResourcesCreativeModeTab.ORES_AND_MINERALS)
+                    , "Chem. Formula: ", "P"));
+
+    // Black Phosphorus (layered material)
+    public static final RegistryObject<Item> BLACK_PHOSPHORUS = ITEMS.register("black_phosphorus",
+            () -> new MineralWithToolTipItem(new Item.Properties().tab(ModResourcesCreativeModeTab.ORES_AND_MINERALS)
+                    , "Chem. Formula: ", "P₄"));
+
+    // Potassium (solid metal)
+    public static final RegistryObject<Item> POTASSIUM = ITEMS.register("potassium_ingot",
+            () -> new MineralWithToolTipItem(new Item.Properties().tab(ModResourcesCreativeModeTab.ORES_AND_MINERALS)
+                    ,"Chem. Formula: ", "K"));
+
+    // Silicon (solid metalloid)
+    public static final RegistryObject<Item> SILICON_CRYSTAL = ITEMS.register("silicon_crystal",
+            () -> new MineralWithToolTipItem(new Item.Properties().tab(ModResourcesCreativeModeTab.ORES_AND_MINERALS)
+                    ,"Chem. Formula: ", "Si"));
 
     public static final RegistryObject<Item> SILVER = ITEMS.register("silver_ingot",
             () -> new MineralWithToolTipItem(new Item.Properties().tab(ModResourcesCreativeModeTab.ORES_AND_MINERALS)
                     ,"Chem. Formula: ", "Ag"));
 
+    // Sodium
+    public static final RegistryObject<Item> SODIUM = ITEMS.register("sodium_ingot",
+            () -> new MineralWithToolTipItem(new Item.Properties().tab(ModResourcesCreativeModeTab.ORES_AND_MINERALS)
+                    ,"Chem. Formula: ", "Na"));
+
     public static final RegistryObject<Item> SOLID_OXYGEN = ITEMS.register("solid_oxygen",
             () -> new MineralWithToolTipItem(new Item.Properties().tab(ModResourcesCreativeModeTab.ORES_AND_MINERALS)
                     ,"Chem. Formula: ", "O₂"));
 
+    // Sulfur (yellow solid)
+    public static final RegistryObject<Item> SULFUR_SOLID = ITEMS.register("sulfur_crystal",
+            () -> new MineralWithToolTipItem(new Item.Properties().tab(ModResourcesCreativeModeTab.ORES_AND_MINERALS)
+                    , "Chem. Formula: ", "S"));
+
+    // Sulfur Powder
+    public static final RegistryObject<Item> SULFUR_POWDER = ITEMS.register("sulfur_powder",
+            () -> new MineralWithToolTipItem(new Item.Properties().tab(ModResourcesCreativeModeTab.ORES_AND_MINERALS)
+                    , "Chem. Formula: ", "S"));
+
+    // Carnallite
+    public static final RegistryObject<Item> CARNALLITE = ITEMS.register("carnallite",
+            () -> new MineralWithToolTipItem(new Item.Properties().tab(ModResourcesCreativeModeTab.ORES_AND_MINERALS),
+                    "Chem. Formula: ", "KMgCl₃·6(H₂O)"));
+
+    // Dolomite
+    public static final RegistryObject<Item> DOLOMITE = ITEMS.register("dolomite",
+            () -> new MineralWithToolTipItem(new Item.Properties().tab(ModResourcesCreativeModeTab.ORES_AND_MINERALS),
+                    "Chem. Formula: ", "CaMg(CO₃)₂"));
+
+    // Evaporite
+    public static final RegistryObject<Item> EVAPORITE = ITEMS.register("evaporite",
+            () -> new MineralWithToolTipItem(new Item.Properties().tab(ModResourcesCreativeModeTab.ORES_AND_MINERALS),
+                    "Chem. Formula: ", "NaCl"));
+
+    // Magnesite
+    public static final RegistryObject<Item> MAGNESITE = ITEMS.register("magnesite",
+            () -> new MineralWithToolTipItem(new Item.Properties().tab(ModResourcesCreativeModeTab.ORES_AND_MINERALS),
+                    "Chem. Formula: ", "MgCO₃"));
+
+    // Pegmatite
+    public static final RegistryObject<Item> PEGMATITE = ITEMS.register("pegmatite",
+            () -> new Item(new Item.Properties().tab(ModResourcesCreativeModeTab.ORES_AND_MINERALS)));
+
+    // Phosphorite
+    public static final RegistryObject<Item> PHOSPHORITE = ITEMS.register("phosphorite",
+            () -> new MineralWithToolTipItem(new Item.Properties().tab(ModResourcesCreativeModeTab.ORES_AND_MINERALS),
+                    "Chem. Formula: ", "Ca₅(PO₄)F₃"));
+
+    // Bauxite
     public static final RegistryObject<Item> BAUXITE = ITEMS.register("raw_bauxite",
             () -> new MineralWithToolTipItem(new Item.Properties().tab(ModResourcesCreativeModeTab.ORES_AND_MINERALS)
                     ,"Chem. Formula: ", "Al₂H₂O₄"));
