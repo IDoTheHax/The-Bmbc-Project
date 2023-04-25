7e7f8c9219c5d3d58aae04010ec1742c7803b1a5
IDoTheHax <mm.mcleod-illert@gmail.com>
2023-04-25 18:25:24 +0200

    - Added Underground village Not complete!!!

--- src/main/java/net/matty/bmbc/BetterMineBetterCraft.java
+++ src/main/java/net/matty/bmbc/BetterMineBetterCraft.java
@@ -30,8 +30,6 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-
-
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(BetterMineBetterCraft.MOD_ID)
 public class BetterMineBetterCraft {
